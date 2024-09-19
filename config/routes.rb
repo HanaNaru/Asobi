1f01d27dba035d1f00823a71a8b307047143555f
HanashiroNarumi <168202959+HanaNaru@users.noreply.github.com>
2024-09-19 14:11:53 +0000

    Initial Commit3 user.mypage.update end

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   resources :post_images
   
   root to: "homes#top"
