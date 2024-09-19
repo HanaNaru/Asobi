4bcefc28e65b9858b82750e2e119d6b0390e1ff4
HanashiroNarumi <168202959+HanaNaru@users.noreply.github.com>
2024-09-19 15:52:51 +0000

    Initial Commit4 comment end

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,12 +16,13 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def index
     @post_images = PostImage.all
   end
-  
+
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
